854e318c1b97b38ec0ed4657dca41b2d0d66a564
Ben <benshanahan1@gmail.com>
2017-04-18 16:35:25 -0400

    added server shutdown script

--- server.py
+++ server.py
@@ -48,7 +48,20 @@ def start():
     cherrypy.engine.start()
     cherrypy.engine.block()
 
+def print_pid(path):
+    pid = os.getpid()
+    try:
+        pidfile = open(path,"wb")
+        output = "dronesvrpid='" + str(pid) + "'"
+        pidfile.write(output)  # write PID to text file
+    finally:
+        try:
+            pidfile.close()
+        except:
+            pass
+
 
 
 if __name__ == "__main__":
+    print_pid(Configuration.SERVER_PID_PATH)  # save Python PID
     start()
