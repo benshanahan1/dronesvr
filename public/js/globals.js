a20599768d402c0162197a734c7e95bb18c850d1
Ben <benshanahan1@gmail.com>
2017-05-03 18:48:26 -0400

    update logout js order

--- public/js/globals.js
+++ public/js/globals.js
@@ -34,6 +34,6 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        window.location.replace("logout");  // server-side log-out
     });
-    window.location.replace("logout");  // redirect user to web app
 }
